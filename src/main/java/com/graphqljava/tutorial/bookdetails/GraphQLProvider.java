96d9c84496f09911e5a10f635c7992c4acc00960
mbrunetto <matteo.brunetto@lastminute.com>
2020-04-24 15:51:09 +0200

    create GraphQL Provider with empty schema

---
+++ src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLProvider.java
@@ -0,0 +1,35 @@
+package com.graphqljava.tutorial.bookdetails;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import java.io.IOException;
+import java.net.URL;
+import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphQLProvider {
+  private GraphQL graphQL;
+
+  @Bean
+  public GraphQL graphQL() {
+    return graphQL;
+  }
+
+  @PostConstruct
+  public void init() throws IOException {
+    URL url = Resources.getResource("schema.graphqls");
+    String sdl = Resources.toString(url, Charsets.UTF_8);
+    GraphQLSchema graphQLSchema = buildSchema(sdl);
+
+    this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+  }
+
+  private GraphQLSchema buildSchema(String sdl) {
+    // TODO: we will create the schema here later
+    return null;
+  }
+}
