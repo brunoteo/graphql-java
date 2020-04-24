1104f8d56b6109446e50f79f90ec648031df2377
mbrunetto <matteo.brunetto@lastminute.com>
2020-04-24 16:05:30 +0200

    add a new DataFetchers

--- src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLProvider.java
+++ src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLProvider.java
@@ -1,17 +1,28 @@
 package com.graphqljava.tutorial.bookdetails;
 
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import java.io.IOException;
 import java.net.URL;
 import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GraphQLProvider {
+  //FIXME remove the autowired
+  @Autowired
+  private GraphQLDataFetchers graphQLDataFetchers;
+
   private GraphQL graphQL;
 
   @Bean
@@ -29,7 +40,18 @@ public class GraphQLProvider {
   }
 
   private GraphQLSchema buildSchema(String sdl) {
-    // TODO: we will create the schema here later
-    return null;
+    TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+    RuntimeWiring runtimeWiring = buildWiring();
+
+    return new SchemaGenerator().makeExecutableSchema(typeRegistry, runtimeWiring);
+  }
+
+  private RuntimeWiring buildWiring() {
+    return RuntimeWiring.newRuntimeWiring()
+        .type(newTypeWiring("Query")
+            .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+        .type(newTypeWiring("Book")
+            .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
+        .build();
   }
 }
